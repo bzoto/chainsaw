31e2cd1828e4aab8b8367c1d44db2dd5edcbc984
bzoto <matteo.pradella@gmail.com>
2015-03-05 16:37:13 +0100

    +

--- examples/minusY.rkt
+++ examples/minusY.rkt
@@ -4,6 +4,8 @@
 (define G1 (build-grammar
              '((E -> ((E - - a T)
                       (E - a T)
+                      (E - a) 
+                      (E - - a) 
                       (a)
                       (- a)
                       (- a x a)
@@ -20,13 +22,30 @@
                       (x - a x -a)
                       (x a x -a))))
              '(E T)))
-                      
+ (define G2 (build-grammar
+             '((S -> ((E) (a T) (- a T)))
+               (E -> ((E - - a T)
+                      (E - a T)
+                      (E - a) 
+                      (E - - a) 
+                      (a)
+                      (- a)
+                      (- a T)
+                      (a T)))
+               (T -> (
+                      (x - a T)
+                      (x a T)
+                      (x - a)
+                      (x a)
+                      )))
+             '(E T S)))                     
                    
-                  
+                 
+(define H 1)
 
-(define grammchains (grammatical-chains G1 'E 1 25 5))
+(define grammchains (grammatical-chains G2 'S H 26 5))
 (show-chains grammchains)
-(define simple-chains (chains-as-set (chains G1 'E 1 100)))
+(define simple-chains (chains-as-set (chains G2 'S H 100)))
 
 
 (time
